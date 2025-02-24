4efe503b895096d7975f0cc3a64bf56d67f49ba2
git <webmaster@shliaoxiang.com>
2025-02-24 14:45:49 +0800

    update merge khjde1207/tesseract_ocr

--- ios/Classes/SwiftFlutterTesseractOcrPlugin.swift
+++ ios/Classes/SwiftFlutterTesseractOcrPlugin.swift
@@ -10,30 +10,19 @@ public class SwiftFlutterTesseractOcrPlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        //initializeTessData()
+        initializeTessData()
         if call.method == "extractText" {
             
             guard let args = call.arguments else {
                 result("iOS could not recognize flutter arguments in method: (sendParams)")
                 return
             }
-    // Move data from bundle to documents directory /tessdata
-    let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-    // this directory will contain our traineddata and is what we will pass to the data source
-    let tessData = documentsURL!.appendingPathComponent("tessdata")
-    // setup the data source class
-    struct MyDataSource: LanguageModelDataSource {
-        let pathToTrainedData: String
-    }
-
-    let dataSource = MyDataSource(pathToTrainedData: tessData.path)
+            
             let params: [String : Any] = args as! [String : Any]
             let language: String? = params["language"] as? String
-            var  swiftyTesseract: SwiftyTesseract;
-            if(language != nil){
-                swiftyTesseract = SwiftyTesseract(language: .custom (language as String!), dataSource: dataSource)
-            } else {
-                swiftyTesseract = SwiftyTesseract(language: .english, dataSource: dataSource)
+            var swiftyTesseract = SwiftyTesseract(language: .english)
+            if let language {
+                swiftyTesseract = SwiftyTesseract(language: .custom(language))
             }
             let  imagePath = params["imagePath"] as! String
             guard let image = UIImage(contentsOfFile: imagePath)else { return }
